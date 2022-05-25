c73a38cd8d4901f550a5b378a3e359195c9869d8
刘刚 <2cevening@gmail.com>
2022-05-26 00:33:13 +0800

    fixed-updated(shop-user):添加主函数启动

--- shop-user/src/main/java/com/liugang/alibabacloud/user/UserStater.java
+++ shop-user/src/main/java/com/liugang/alibabacloud/user/UserStater.java
@@ -1,11 +1,17 @@
 package com.liugang.alibabacloud.user;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+//开启事务支持
 @EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan(value = { "com.liugang.alibabacloud.user.mapper" })
 public class UserStater {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserStater.class,args);
+    }
 }
