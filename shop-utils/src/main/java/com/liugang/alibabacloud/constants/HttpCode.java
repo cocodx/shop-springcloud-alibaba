2a2d75d5c9073c5cb28e662d75e8b8118fd57f82
刘刚 <2cevening@gmail.com>
2022-05-25 23:50:00 +0800

    fixed-update(shop-utils):添加工具子模块

---
+++ shop-utils/src/main/java/com/liugang/alibabacloud/constants/HttpCode.java
@@ -0,0 +1,17 @@
+package com.liugang.alibabacloud.constants;
+
+/**
+ * http状态码
+ */
+public class HttpCode {
+
+    /**
+     * 成功的状态码
+     */
+    public static final int SUCCESS = 200;
+
+    /**
+     * 错误状态码
+     */
+    public static final int FAILURE = 500;
+}
---
+++ shop-utils/src/main/java/com/liugang/alibabacloud/exception/RestCtrlExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.liugang.alibabacloud.exception;
+
+import com.liugang.alibabacloud.constants.HttpCode;
+import com.liugang.alibabacloud.resp.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+/**
+ * 全局异常处理器
+ */
+@RestControllerAdvice
+public class RestCtrlExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(RestCtrlExceptionHandler.class);
+
+    /**
+     * 全局异常处理，统一返回状态码
+     */
+    @ExceptionHandler(Exception.class)
+    public Result<String> handleException(Exception e){
+        logger.error("服务器抛出了异常：{}",e);
+        return new Result<String>(HttpCode.FAILURE,"执行失败",e.getMessage());
+    }
+}
